5b721affec8a44b329372bf501bc4f9b59733644
Jan <jan.witek88@gmail.com>
2021-03-21 12:56:40 +0100

    Fixed bugs, sometimes doesnt recognize draw

--- app/src/main/java/pollub/ism/lab04/MainActivity.java
+++ app/src/main/java/pollub/ism/lab04/MainActivity.java
@@ -138,19 +138,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        savedInstanceState.putInt("roundCount", roundCount);
-        savedInstanceState.putInt("player1Points", player1Points);
-        savedInstanceState.putInt("player2Points", player2Points);
-        savedInstanceState.putBoolean("player1Turn", player1Turn);
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("player1Points", player1Points);
+        outState.putInt("player2Points", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
     }
 
     @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
         roundCount = savedInstanceState.getInt("roundCount");
         player1Points = savedInstanceState.getInt("player1Points");
         player2Points = savedInstanceState.getInt("player2Points");
