7cd6d05aac3174dfe6f91ee8905b3fabf1ecec60
Jan <jan.witek88@gmail.com>
2021-03-21 11:26:20 +0100

    board ready

--- app/src/main/java/pollub/ism/lab04/MainActivity.java
+++ app/src/main/java/pollub/ism/lab04/MainActivity.java
@@ -1,9 +1,18 @@
 package pollub.ism.lab04;
 
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView playerOneScore, playerTwoScore, playerStatus;
+    private Button[] buttons = new Button[9];
+    private Button resetGame;
+
+    private int playerOneScoreCount, getPlayerTwoScoreCount, roundCount;
+    boolean activePlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
